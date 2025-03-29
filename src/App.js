76bace0452e1f015576a2fdcc2462fc1b40a58c6
Kalleb <gk26122003@gmail.com>
2025-03-29 12:06:25 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="A">
       <header className="App-header">
         <h1>Sistema de Geração de Grupos</h1>
       </header>
